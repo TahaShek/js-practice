80202391e1331277e8bdfc846f4fd5d14c372c67
TahaSekk <sohailtaha128@gmail.com>
2022-06-30 06:02:43 +0500

    coomit

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 let access=(num1)=>{
-    if(num1===10){
-        console.log('we here')
-    }
-    else{
-        console.log('nkal yaha sei ')
-    }
+    // if(num1===10){
+    //     console.log('we here')
+    // }
+    // else if(num1 >=9){
+    //     console.log(' welcome here')
+    // }
+    // else if(num1===8){
+    //     console.log(' welcome here to dashborad')
+    // }
+    // else if(num1===7){
+    //     console.log(' ok boos')
+    // }
+    // // else{
+    // //     console.log(`you have enter ${num1}`)
+    // // }
 
+    if(num1===5){
+        console.log('hello world')
+    return
+    }
+    if(num1==34){
+        console.log(`you are eligable ${num1}`)
+    return
+    }
+    console.log('hi')
 }
-access(5)
+access(34);
+
